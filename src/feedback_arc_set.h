0abf13c5f58cf71ed2bb6453465790bae043e48c
Alan <alancleary@comcast.net>
2014-06-28 22:38:29 -0600

    Added the FAS ILP

---
+++ src/feedback_arc_set.h
@@ -0,0 +1,7 @@
+#ifndef FEEDBACK_ARC_SET_H
+#define FEEDBACK_ARC_SET_H
+
+template<typename T_in,typename T_out >
+T_out integer_linear_program( DirectedScaffoldGraph g, T_out* fas )
+
+#endif /* FEEDBACK_ARC_SET_H */
