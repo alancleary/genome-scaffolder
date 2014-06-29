419fe4b29745ce8591bd71ad31028921bc981aa6
Alan <alancleary@comcast.net>
2014-06-29 15:52:43 -0600

    Feedback arc set stuff is now in error calculator

--- src/feedback_arc_set.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef FEEDBACK_ARC_SET_H
-#define FEEDBACK_ARC_SET_H
-
-#include <ilcplex/ilocplex.h>
-
-template<typename T_in,typename T_out>
-T_out integer_linear_program( const DirectedScaffoldGraph&, T_out* );
-
-#endif /* FEEDBACK_ARC_SET_H */
